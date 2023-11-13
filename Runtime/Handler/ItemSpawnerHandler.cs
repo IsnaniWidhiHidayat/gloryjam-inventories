9c054bca160a89ac98be53b0c8b2e62318c06094
Widhi <isnani.hidayat@rollingglory.com>
2023-11-13 21:58:30 +0700

    chore(ItemSpawner) : update item spawner

--- Runtime/Handler/ItemSpawnerHandler.cs
+++ Runtime/Handler/ItemSpawnerHandler.cs
@@ -6,8 +6,19 @@ namespace GloryJam.Inventories
     [Serializable]
     public abstract class ItemSpawnerHandler : ItemComponentHandler
     {
+        public GameObject Object;
+
         #region methods
-        public abstract GameObject Spawn();
+        public virtual GameObject Spawn(ItemStack stack){
+            var clone = GameObject.Instantiate(Object);
+
+            if(stack != null) {
+                var itemObject = clone.GetComponent<ItemObject>();
+                if(itemObject == null) itemObject = clone.AddComponent<ItemObject>();
+                itemObject.SetStack(stack);
+            }
+            return clone;
+        }
         #endregion
     }
 }
