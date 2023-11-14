91f4df081cb14eb02ad3829b2c828428de474012
Widhi <isnani.hidayat@rollingglory.com>
2023-11-14 23:21:58 +0700

    update

--- Runtime/Object/ItemObjectStack.cs
+++ Runtime/Object/ItemObjectStack.cs
@@ -4,33 +4,39 @@ using UnityEngine;
 using Sirenix.OdinInspector;
 #endif
 
-
 namespace GloryJam.Inventories
 {
     [AddComponentMenu("Glory Jam/Inventory/" + nameof(ItemObjectStack))]
     public class ItemObjectStack : MonoBehaviour ,IItemObjectStack
     {
         #region const
-        protected const string grpStack = "Stack";
+        protected const string grpItem = "Item";
         protected const string grpConfig = "Config";
         protected const string grpRequired = "Required";
         protected const string grpRuntime = "Runtime";
         #endregion
 
-        #region property
-        public ItemStack stack => _stack;
-        #endregion
-
-        #region private
+        #region fields
         #if ODIN_INSPECTOR
-        [ShowInInspector,BoxGroup(grpStack),HideLabel]
+        [ShowInInspector,BoxGroup(grpItem),HideLabel,HideDuplicateReferenceBox,HideReferenceObjectPicker]
+        #endif
+        protected Item _item;
+
+        #if ODIN_INSPECTOR
+        [ShowInInspector,BoxGroup(grpItem),HideLabel,HideDuplicateReferenceBox,HideReferenceObjectPicker]
         #endif
         protected ItemStack _stack;
+        #endregion
+
+        #region property
+        public Item item => _stack?.item;
+        public ItemStack stack => _stack;
         #endregion
 
         #region methods
         public void SetStack(ItemStack stack){
             _stack = stack;
+            _item = stack?.item;
         }
         #endregion
     }
